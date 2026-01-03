a33bcefa675f1cb84391666cd117d5ca9f3d2d96
lucasjanta <lucastyle2014@gmail.com>
2026-01-03 18:52:01 -0300

    player movement

---
+++ scripts/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+class_name Player
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+@export var speed : float = 150.0
+@export var gravity_force : float = 900.0
+
+enum State { IDLE, PICK, WALK }
+var state: State = State.IDLE
+
+func _physics_process(delta):
+	_apply_gravity(delta)
+	_process_state(delta)
+	move_and_slide()
+		
+	
+func _apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity_force * delta
+	else:
+		velocity.y = 0
+		
+func _unhandled_input(event):
+	var horizontal_movement = Input.get_axis("left", "right")
+	if horizontal_movement == 0:
+		state = State.IDLE
+		animated_sprite_2d.play("idle")
+	else:
+		state = State.WALK
+		animated_sprite_2d.play("walk")
+	
+func _process_state(delta):
+	match state:
+		State.IDLE:
+			velocity.x = move_toward(velocity.x, 0, speed)
+			
+		State.WALK:
+			var dir = Input.get_axis("left", "right")
+			velocity.x = speed * dir
+			if dir > 0:
+				animated_sprite_2d.flip_h = false
+			elif dir < 0:
+				animated_sprite_2d.flip_h = true
+				
